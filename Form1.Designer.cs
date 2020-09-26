2cfeccfa51ad9ae96e7552604a06a2002b9b0ba9
Lorenzo <lorenzomc6@gmail.com>
2020-09-25 23:48:29 -0500

    Correcccion de Interfaz

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,59 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SelectDestino = new System.Windows.Forms.Button();
+            this.Sincronizar = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
             this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.InfOrigen = new System.Windows.Forms.Label();
+            this.InfoDestino = new System.Windows.Forms.Label();
+            this.SelectOrigen = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // SelectDestino
             // 
-            this.button1.Location = new System.Drawing.Point(45, 52);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Seleccionar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.SelectDestino.Location = new System.Drawing.Point(367, 399);
+            this.SelectDestino.Name = "SelectDestino";
+            this.SelectDestino.Size = new System.Drawing.Size(75, 23);
+            this.SelectDestino.TabIndex = 1;
+            this.SelectDestino.Text = "Seleccionar";
+            this.SelectDestino.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // Sincronizar
             // 
-            this.button2.Location = new System.Drawing.Point(45, 130);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Seleccionar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Sincronizar.Location = new System.Drawing.Point(45, 72);
+            this.Sincronizar.Name = "Sincronizar";
+            this.Sincronizar.Size = new System.Drawing.Size(75, 23);
+            this.Sincronizar.TabIndex = 2;
+            this.Sincronizar.Text = "Sincronizar";
+            this.Sincronizar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // Borrar
             // 
-            this.button3.Location = new System.Drawing.Point(164, 399);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Sincronizar";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(367, 399);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Borrar";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(164, 52);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(184, 328);
-            this.checkedListBox1.TabIndex = 6;
+            this.Borrar.Location = new System.Drawing.Point(45, 112);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(75, 23);
+            this.Borrar.TabIndex = 3;
+            this.Borrar.Text = "Borrar";
+            this.Borrar.UseVisualStyleBackColor = true;
             // 
             // checkedListBox2
             // 
@@ -90,37 +73,54 @@
             this.checkedListBox2.Size = new System.Drawing.Size(188, 328);
             this.checkedListBox2.TabIndex = 6;
             // 
-            // label1
+            // InfOrigen
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Origen";
+            this.InfOrigen.AutoSize = true;
+            this.InfOrigen.Location = new System.Drawing.Point(164, 34);
+            this.InfOrigen.Name = "InfOrigen";
+            this.InfOrigen.Size = new System.Drawing.Size(43, 15);
+            this.InfOrigen.TabIndex = 7;
+            this.InfOrigen.Text = "Origen";
             // 
-            // label2
+            // InfoDestino
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 101);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 15);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Destino";
+            this.InfoDestino.AutoSize = true;
+            this.InfoDestino.Location = new System.Drawing.Point(367, 34);
+            this.InfoDestino.Name = "InfoDestino";
+            this.InfoDestino.Size = new System.Drawing.Size(47, 15);
+            this.InfoDestino.TabIndex = 8;
+            this.InfoDestino.Text = "Destino";
+            // 
+            // SelectOrigen
+            // 
+            this.SelectOrigen.Location = new System.Drawing.Point(164, 399);
+            this.SelectOrigen.Name = "SelectOrigen";
+            this.SelectOrigen.Size = new System.Drawing.Size(75, 23);
+            this.SelectOrigen.TabIndex = 0;
+            this.SelectOrigen.Text = "Seleccionar";
+            this.SelectOrigen.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(164, 52);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(184, 328);
+            this.checkedListBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 506);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(635, 502);
+            this.Controls.Add(this.InfoDestino);
+            this.Controls.Add(this.InfOrigen);
             this.Controls.Add(this.checkedListBox2);
             this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Borrar);
+            this.Controls.Add(this.Sincronizar);
+            this.Controls.Add(this.SelectDestino);
+            this.Controls.Add(this.SelectOrigen);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -130,15 +130,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button SelectDestino;
+        private System.Windows.Forms.Button Sincronizar;
+        private System.Windows.Forms.Button Borrar;
         private System.Windows.Forms.CheckedListBox checkedListBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label InfOrigen;
+        private System.Windows.Forms.Label InfoDestino;
+        private System.Windows.Forms.Button SelectOrigen;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
 
